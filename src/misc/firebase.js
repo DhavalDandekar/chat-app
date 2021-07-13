68ded2a4d62a988bc032304d26d47ee1b3aa8dce
DhavalDandekar <dandekardhaval@gmail.com>
2021-07-13 12:35:28 +0530

    button to create new chat room

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import 'firebase/storage'
 
 const config = {
   apiKey: 'AIzaSyCJpLvhE021A5_bxQjtSLfawY8a8mGj9zM',
@@ -15,3 +16,4 @@ const app = firebase.initializeApp(config)
 
 export const auth = app.auth()
 export const database = app.database()
+export const storage = app.storage()
