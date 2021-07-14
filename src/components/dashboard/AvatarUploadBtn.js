f5e5e94a52144717819edd38a5dcb6bb273f5fae
DhavalDandekar <dandekardhaval@gmail.com>
2021-07-14 17:13:22 +0530

    display chat done and added realtime presence

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -5,6 +5,7 @@ import { useModalState } from '../../misc/custom-hooks'
 import { database, storage } from '../../misc/firebase'
 import { useProfile } from '../../context/profile.context'
 import ProfileAvatar from '../ProfileAvatar'
+import { getUserUpdates } from '../../misc/helpers'
 
 const fileInputTypes = '.png, .jpeg, .jpg'
 
@@ -63,11 +64,14 @@ const AvatarUploadBtn = () => {
 
       const downloadUrl = await uplooadAvatarResult.ref.getDownloadURL()
 
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar')
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      )
 
-      userAvatarRef.set(downloadUrl)
+      await database.ref().update(updates)
 
       setIsLoading(false)
       Alert.info('Avatar has been uploaded', 4000)
