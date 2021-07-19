d4c5bc6dbb2e7c86a3fea2c9f791896c3ed2e367
DhavalDandekar <dandekardhaval@gmail.com>
2021-07-19 20:50:59 +0530

    like function added

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { database } from '../../../misc/firebase'
+import { Alert } from 'rsuite'
+import { auth, database } from '../../../misc/firebase'
 import { transformToArrWithId } from '../../../misc/helpers'
 import MessageItem from './MessageItem'
 
@@ -29,12 +30,72 @@ const Messages = () => {
     }
   }, [chatId])
 
+  const handleAdmin = useCallback(
+    async uid => {
+      const adminsRef = database.ref(`/rooms/${chatId}/admins`)
+
+      let alertMsg
+
+      await adminsRef.transaction(admins => {
+        if (admins) {
+          if (admins[uid]) {
+            admins[uid] = null
+            alertMsg = 'Admin permission Removed'
+          } else {
+            admins[uid] = true
+            alertMsg = 'Admin permission Granted'
+          }
+        }
+        return admins
+      })
+
+      Alert.info(alertMsg, 4000)
+    },
+    [chatId]
+  )
+
+  const handleLike = useCallback(async msgId => {
+    const { uid } = auth.currentUser
+
+    const messageRef = database.ref(`/messages/${msgId}`)
+
+    let alertMsg
+
+    await messageRef.transaction(msg => {
+      if (msg) {
+        if (msg.likes && msg.likes[uid]) {
+          msg.likeCount -= 1
+          msg.likes[uid] = null
+          alertMsg = 'Like Removed'
+        } else {
+          msg.likeCount += 1
+          if (!msg.likes) {
+            msg.likes = {}
+          }
+
+          msg.likes[uid] = true
+          alertMsg = 'Like Added'
+        }
+      }
+      return msg
+    })
+
+    Alert.info(alertMsg, 4000)
+  }, [])
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Messages yet</li>}
 
       {canShowMessages &&
-        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+        messages.map(msg => (
+          <MessageItem
+            key={msg.id}
+            message={msg}
+            handleAdmin={handleAdmin}
+            handleLike={handleLike}
+          />
+        ))}
     </ul>
   )
 }
